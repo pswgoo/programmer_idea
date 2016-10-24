a0f35c19882cadff0e6af55eaad12952a6c33cb3
pswgoo <shengwenpeng@accutarbio.com>
2016-10-24 23:13:50 +0800

    changed arch

--- compiler/compiler_basic/cl_parser.h
+++ compiler/compiler_basic/cl_parser.h
@@ -23,56 +23,7 @@ protected:
 	std::vector<std::unique_ptr<ClAstNode>> children_;
 };
 
-class ClProg1Node: public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
 
-};
-class ClProg2Node : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class ClProg3Node : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class ClStmtNode : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class ClStmt1Node : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class ClFuncDefNode : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class ClFuncDef1Node : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class TypeNode : public ClAstNode {
-public:
-	TypeNode(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent):ClAstNode(lexer, move(parent)) {}
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-};
-class ClExprNode : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
-class ClDeclNode : public ClAstNode {
-public:
-	virtual std::unique_ptr<ClAstNode> Parse(Lexer& lexer, std::unique_ptr<ClAstNode>&& parent);
-
-};
 
 class ClParser {
 public:
