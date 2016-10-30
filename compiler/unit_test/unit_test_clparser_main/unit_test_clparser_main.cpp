713919616ea1ad9cc6ed52bee9affebb16ebd8d1
pswgoo <shengwenpeng@accutarbio.com>
2016-10-30 21:56:35 +0800

    debug

--- compiler/unit_test/unit_test_clparser_main/unit_test_clparser_main.cpp
+++ compiler/unit_test/unit_test_clparser_main/unit_test_clparser_main.cpp
@@ -10,16 +10,21 @@ int main(int argc, char** argv) {
 	//if a<=b>cd==<(=== = = else cc3 =a1 + bb2/2432*3 + 2 - (-c+d)*((bc2+c)/(342- 4))+psw
 	string test_str = R"DELIM(
 	
+	int xxxx;
+	xxxx = 12 * 4 % 3;	
+
+
 	double a;
 	int b;
 	b = 12;
 	a = 1e-10;
 	double c;
-	c = 12 * 4 + (b / 2) + a;
+	a = c = 12 * 4 % 3+ (b / 2 * 2) + a;
 	double d;
 	d += c + a;
 	bool e;
 	e = d > c;
+
 	)DELIM";
 
 	Lexer lexer;
